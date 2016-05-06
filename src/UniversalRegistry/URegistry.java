df76eda8293e0379eb14df3b7bc1b0e19f849cb3
PierreMassanes <pierre.massanes@gmail.com>
2016-05-06 10:50:31 +0200

    URegistry added

---
+++ src/UniversalRegistry/URegistry.java
@@ -0,0 +1,17 @@
+package UniversalRegistry;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface URegistry extends Remote {
+
+    void bind(String key, Object object) throws AlreadyBoundException, RemoteException;
+    void rebind(String key, Object object) throws RemoteException;
+    Object get(String key) throws RemoteException;
+    List<String> list() throws RemoteException;
+    List<Object> getLastObjects(int until) throws RemoteException;
+    List<String> getLastKeys(int until) throws RemoteException;
+    List<String> getPopularKey(int until) throws RemoteException;
+}
