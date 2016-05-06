255d36510f4e6010c68ab451766d5987d7aec435
PierreMassanes <pierre.massanes@gmail.com>
2016-05-06 14:17:02 +0200

    Donnee Impl + Donnee

--- src/data/Donnee.java
+++ src/data/Donnee.java
@@ -1,8 +1,12 @@
 package data;
 
+import java.io.Serializable;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * Created by user on 05/05/16.
  */
-public interface Donnee {
+public interface Donnee extends Remote {
     String toString();
 }
